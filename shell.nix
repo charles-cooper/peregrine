d8a5fdf215c24aa8ce8e8cdc0d4e7894fec129f5
Charles Cooper <cooper.charles.m@gmail.com>
2016-03-14 11:40:50 -0400

    Change to auto-upgrading lts

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? (import <nixpkgs> {}) }:
 let
-  drv = pkgs.haskell.packages.lts-5_5.callPackage (import ./default.nix) {};
+  drv = pkgs.haskell.packages.lts.callPackage (import ./default.nix) {};
 in
   if pkgs.lib.inNixShell then drv.env else drv
